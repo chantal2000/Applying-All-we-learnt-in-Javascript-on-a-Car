033f34e878320f45e34ac663a76d7a5523482edc
chantal2000 <nniyonkuruchantal@gmail.com>
2021-05-06 07:42:28 +0300

    javascript revision

--- car.js
+++ car.js
@@ -11,89 +11,118 @@ let x=function(){     //Anonymous function and declaring a function as a variabl
     console.log(carname2); 
     console.log(carName);
 }
-// console.log(carname2);// carName2 is local scope which means that it can't be accessed outside the block
-// x()
-
-// function car(carname,make,capacity){  // function creation ' function name is Car(named function) with parameters name,make,capacity and keyword function 
-//     console.log(carname); // output statement
-// }
-//   car("Mazda","Toyota",5) // arguments Mazda,Toyota and 5
-//                        // datatype  of 5 is a number
-//                         // datatype of MAzda and Toyota is string
-//                         //function call
-
-//  (function IIFE(){
-//    const capacity=5;           // const is a variable that can't be reassigned or change
-//    console.log(capacity);
-//  } () )                 // Immediately invoked function 
-
-// let  maxfare=(capacity,fare)=>capacity*fare;
-// console.log(maxfare(5,100))               //Arrow function
-//                                           //let variable which can be changed or not
-
-// function start() {
-//     let carStart = "Voooooooooo"
-//     console.log(carStart)            // closure where function stop has ability to remember what is in start and it is also a nested function
-//     function stop() {
-//         let carStop = carStart + " proooo"  //concatination
-//         console.log(carStop)
-
-//     }
-//     stop()
-// }
-// start()
-
-// function buyCar() {
-//     const price = 50000000
-//     console.log(price)
-// }
-// function sellCar(sellingPrice, callback) {
-//     setTimeout(() => {
-//         console.log("Call back Function");
-//     }, 3000);                                // setTimeout
-    // console.log(sellingPrice)
-
-// }
-// sellCar(2000000, buyCar());   // Call back function : buyCar is used as an argument of sellCar
-
-// class Car {     //class creation called Car
-//     constructor(registration, model) {
-//         this.registration = registration;   //this keyword that define an object they belong to
-//         this.model = model;
-//         this.carInfo = function () {    // method which is function used as a property of object
-//             return " Our car has" + " " + this.registration + "" + "number  " + " and" + " " + this.model
-//         }
-//         Car.prototype.refillPetrol = 40;    // using prototype to add an other object property
-//     }
-// }
-// let car1 = new Car("KC5678", "cx567") // creation of object car1 using new keyword 
-// console.log(car1.registration)     //access
-// console.log(car1.carInfo())
-// console.log(car1.refillPetrol)
+console.log(carname2);// carName2 is local scope which means that it can't be accessed outside the block
+x()
 
 
-// let promise=new Promise((resolve,reject)=>{   //promises
-//     let carCrushes=true;      //boolean datatype
-//     if(carCrushes){
-//         resolve("pay back")
-//     }
-//     else{
-//         reject("Buy another one")    //conditional if else statement
-//     }
 
-// }).then((message)=>{
-//     console.log('Insurance will pay back');
-// }).catch((message)=>{
-//     console.log("can't be compessated")
-// })
-//  async function cleanCar(){                //Asynchronous function
-//     let y=setInterval(() => {        //setinterval and clear it
-//         console.log("Let'st start cleaning");
-//         clearInterval(y);
-//     }, 2000);
-//     console.log("Wuw!! We are done")
-// }
-// cleanCar()
+
+
+
+function car(carname,make,capacity){  // function creation ' function name is Car(named function) with parameters name,make,capacity and keyword function 
+    console.log(carname); // output statement
+}
+  car("Mazda","Toyota",5) // arguments Mazda,Toyota and 5
+                       // datatype  of 5 is a number
+                        // datatype of MAzda and Toyota is string
+                        //function call
+
+
+
+
+
+ (function IIFE(){
+   const capacity=5;           // const is a variable that can't be reassigned or change
+   console.log(capacity);
+ } () )                 // Immediately invoked function 
+
+let  maxfare=(capacity,fare)=>capacity*fare;
+console.log(maxfare(5,100))               //Arrow function
+                                          //let variable which can be changed or not
+
+
+
+
+function start() {
+    let carStart = "Voooooooooo"
+    console.log(carStart)            // closure where function stop has ability to remember what is in start and it is also a nested function
+    function stop() {
+        let carStop = carStart + " proooo"  //concatination
+        console.log(carStop)
+
+    }
+    stop()
+}
+start()
+
+
+
+
+
+function buyCar() {
+    const price = 50000000
+    console.log(price)
+}
+function sellCar(sellingPrice, callback) {
+    setTimeout(() => {
+        console.log("Call back Function");
+    }, 3000);                                // setTimeout
+    console.log(sellingPrice)
+
+}
+sellCar(2000000, buyCar());   // Call back function : buyCar is used as an argument of sellCar
+
+
+
+
+
+
+class Car {     //class creation called Car
+    constructor(registration, model) {
+        this.registration = registration;   //this keyword that define an object they belong to
+        this.model = model;
+        this.carInfo = function () {    // method which is function used as a property of object
+            return " Our car has" + " " + this.registration + "" + "number  " + " and" + " " + this.model
+        }
+        Car.prototype.refillPetrol = 40;    // using prototype to add an other object property
+    }
+}
+let car1 = new Car("KC5678", "cx567") // creation of object car1 using new keyword 
+console.log(car1.registration)     //access
+console.log(car1.carInfo())
+console.log(car1.refillPetrol)
+
+
+
+
+
+
+
+let promise=new Promise((resolve,reject)=>{   //promises
+    let carCrushes=true;      //boolean datatype
+    if(carCrushes){
+        resolve("pay back")
+    }
+    else{
+        reject("Buy another one")    //conditional if else statement
+    }
+
+}).then((message)=>{
+    console.log('Insurance will pay back');
+}).catch((message)=>{
+    console.log("can't be compessated")
+})
+ async function cleanCar(){                //Asynchronous function
+    let y=setInterval(() => {        //setinterval and clear it
+        console.log("Let'st start cleaning");
+        clearInterval(y);
+    }, 2000);
+    console.log("Wuw!! We are done")
+}
+cleanCar()
+
+
+
 
 
 let car = {         //object car
@@ -102,34 +131,43 @@ let car = {         //object car
 }
 console.log(car.fname); // Dot method to access an object's property
 console.log(car['lname']); //Bracket method to access an object's property
-// let { fname, lname } = car    //Object destructuring
-// console.log(fname);
-
-// let { fname, ...rest } = car    //using rest keyword 
-// console.log(rest.lname);
-// console.log(fname)
+let { fname, lname } = car    //Object destructuring
+console.log(fname);
 
 
-// for (let [key, value] of Object.entries(car)) {    //for loop
-//     console.log(`${key}:${value}`)
-// }
-// for (key in car) {     //for in loop
-//     console.log(key)
-// }
+
+let { fname, ...rest } = car    //using rest keyword 
+console.log(rest.lname);
+console.log(fname)
+
+
+
+
+for (let [key, value] of Object.entries(car)) {    //for loop
+    console.log(`${key}:${value}`)
+}
+
+
+for (key in car) {     //for in loop
+    console.log(key)
+}
+
 
 
 let car = ["Mini", "Bus"];
-// let [fname, lname] = car     // Array destructuring
-// console.log(fname)
+let [fname, lname] = car     // Array destructuring
+console.log(fname);
+
+
 for(name of car){   //for of loop
     console.log(name);
 }
 
 
 
-// export function Speed(value){  //export module
-//     return speed*3;
-// }
+export function Speed(value){  //export module
+    return speed*3;
+}
 
 
 let carNum=0;
@@ -137,6 +175,8 @@ while(carNum<5){   //While loop
     console.log(carNum);
     carNum++;
 }
+
+
 
 let carNumber=6;   //do while loop
 do{   
@@ -146,12 +186,18 @@ do{
 while(carNumber>0);
 
 
+
+
 let carId='100';
-// let carId2=number(carId)
-// console.log(typeof(carId)); // Explicit coersion
+let carId2=number(carId)
+console.log(typeof(carId)); // Explicit coersion
+
+
 
 let carId2=carId + '';
 console.log(carId2) // implicit coersion
+
+
 
 let carSpeed="50";
 let carspeed2=50;
@@ -159,6 +205,9 @@ console.log(carSpeed==carspeed2);//loosely equal to, Compares only values
 console.log(carSpeed===carspeed2);//strictly equal to,Compares both values and types
 console.log(carSpeed!=carspeed2);//Not loosely equal to 
 console.log(carSpeed!==carspeed2);//Not strictly equal to
+
+
+
 
 var carN=40;      //using switch case
 switch(true){
@@ -169,3 +218,19 @@ switch(true){
        cconsole.log("Not enough")
 }
 
+
+
+function ternaryOp(){        //Ternary operator
+     car=="car"? console.log("true"): console.log("false") 
+}
+ternaryOp()
+
+
+
+
+var strict=function(){   //using strict mode which will give us an error 
+    "use strict",
+    a=30;
+    console.log(a)
+}
+strict()       //function as a value
